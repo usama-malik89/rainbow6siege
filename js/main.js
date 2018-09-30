55ba7b3449b5eb17c61ccb8a1563997d0e2f5c81
Usama <usama89@hotmail.co.uk>
2018-09-30 21:02:47 +0100

    v2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var maps = [
+var casual_maps = [
             "House",
             "Oregon",
             "Hereford_Base",
@@ -19,6 +19,43 @@ var maps = [
             "Villa"
            ]
 
+var ranked_maps = [
+            "Oregon",
+            "Hereford_Base",
+            "Club_House",
+            "Consulate",
+            "Bank",
+            "Chalet",
+            "Kafe_Dostoyevsky",
+            "Border",
+            "Skyscraper",
+            "Coastline",
+            "Theme_Park",
+            "Villa"
+           ]
+
+var all_maps = [
+            "House",
+            "Oregon",
+            "Hereford_Base",
+            "Club_House",
+            "Presidential_Plane",
+            "Consulate",
+            "Bank",
+            "Kanal",
+            "Chalet",
+            "Kafe_Dostoyevsky",
+            "Yacht",
+            "Border",
+            "Favela",
+            "Skyscraper",
+            "Coastline",
+            "Theme_Park",
+            "Tower",
+            "Villa",
+            "Bartlett_University"
+           ]
+
 
 function random_num(list){
     i = Math.floor(Math.random() * list.length);
@@ -26,16 +63,30 @@ function random_num(list){
 }
 
 $(document).ready(function(){
-    y = maps[random_num(maps)]
+    $("#butn").click(function(){
+        if($( "input:checked" ).val() === "allMaps"){
+            map_type(all_maps)
+        }
+        else if($( "input:checked" ).val() === "casualMaps"){
+            map_type(casual_maps)
+        }
+        else if($( "input:checked" ).val() === "rankedMaps"){
+            map_type(ranked_maps)
+        }
+    });
+});
+
+function map_type(list){
+    y = list[random_num(list)]
     ny = y.replace('_', ' ');
     $("#mapName").text(ny);
     $("#img").attr('src', 'assets/' + y + '.jpg');
     
-    $("#btn").click(function(){
+    $("#butn").click(function(){
     $("#img").attr('src', 'assets/' + y + '.jpg');
-        x = maps[random_num(maps)]
+        x = list[random_num(list)]
         nx = x.replace('_', ' ');
         $("#mapName").text(nx);
         $("#img").attr('src', 'assets/' + x + '.jpg');
     });
-});
+}
